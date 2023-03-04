6864869327570ca548842e727ab4c763a81055f5
40476 <63472353+40476@users.noreply.github.com>
2023-03-04 01:20:39 +0000

    e

--- replit.nix
+++ replit.nix
@@ -1,7 +1,8 @@
 
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.openssh_with_kerberos
+  pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.replitPackages.jest
